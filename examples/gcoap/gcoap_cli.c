a1beac04a9ede9f124c9995f5f1afc733cc01573
János Brodbeck <janos.brodbeck@posteo.de>
2020-10-29 19:35:31 +0100

    example/gcoap: add DTLS

--- examples/gcoap/gcoap_cli.c
+++ examples/gcoap/gcoap_cli.c
@@ -30,6 +30,27 @@
 #define ENABLE_DEBUG 0
 #include "debug.h"
 
+#ifdef CONFIG_GCOAP_ENABLE_DTLS
+    #include "tinydtls_keys.h"
+    #include "net/credman.h"
+
+    #define SOCK_DTLS_CLIENT_TAG (2)
+    static const uint8_t psk_id_0[] = PSK_DEFAULT_IDENTITY;
+    static const uint8_t psk_key_0[] = PSK_DEFAULT_KEY;
+
+    static const credman_credential_t credential = {
+        .type = CREDMAN_TYPE_PSK,
+        .tag = SOCK_DTLS_CLIENT_TAG,
+        .params = {
+            .psk = {
+                .key = { .s = psk_key_0, .len = sizeof(psk_key_0) - 1, },
+                .id = { .s = psk_id_0, .len = sizeof(psk_id_0) - 1, },
+            }
+        },
+    };
+#endif
+
+
 static bool _proxied = false;
 static sock_udp_ep_t _proxy_remote;
 static char proxy_uri[64];
@@ -317,6 +338,9 @@ int gcoap_cli_cmd(int argc, char **argv)
         uint8_t open_reqs = gcoap_op_state();
 
         printf("CoAP server is listening on port %u\n", CONFIG_GCOAP_PORT);
+        #ifdef CONFIG_GCOAP_ENABLE_DTLS
+            printf("Connection secured with DTLS\n");
+        #endif
         printf(" CLI requests sent: %u\n", req_count);
         printf("CoAP open requests: %u\n", open_reqs);
         printf("Configured Proxy: ");
@@ -466,5 +490,14 @@ int gcoap_cli_cmd(int argc, char **argv)
 
 void gcoap_cli_init(void)
 {
+#ifdef CONFIG_GCOAP_ENABLE_DTLS
+    int res = credman_add(&credential);
+    if (res < 0 && res != CREDMAN_EXIST) {
+        /* ignore duplicate credentials */
+        printf("Error cannot add credential to system: %d\n", (int)res);
+        return;
+    }
+#endif
+
     gcoap_register_listener(&_listener);
 }
